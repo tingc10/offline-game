9eaff06c6423c94b43854ecd8e40c07c3608fffc
Ting Chen <ting@squareup.com>
2019-08-27 22:09:10 -0400

    Updates

---
+++ precache-manifest.b92740209036c85dcb4838e1fb7889aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3bcd3fd60177e55e722a4c904222ed7",
+    "url": "/offline-game/index.html"
+  },
+  {
+    "revision": "a5913fa7f4a39d61c617",
+    "url": "/offline-game/static/css/main.7cd31703.chunk.css"
+  },
+  {
+    "revision": "bd30d61578803a6de6fb",
+    "url": "/offline-game/static/js/2.8ebd3ea5.chunk.js"
+  },
+  {
+    "revision": "a5913fa7f4a39d61c617",
+    "url": "/offline-game/static/js/main.b819e135.chunk.js"
+  },
+  {
+    "revision": "df4dd1144d437080fdd4",
+    "url": "/offline-game/static/js/runtime~main.94be288d.js"
+  }
+]);
